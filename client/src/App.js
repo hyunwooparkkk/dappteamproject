9784c92d8b4036a515fcd24a6930f06e325e3701
jinwood <dhehdid1@gmail.com>
2021-08-25 19:28:13 +0900

    라이브러리 설치, Home.js 디자인 중

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Create from './components/Create';
-import First from './components/Home';
+import Home from './components/Home';
 import Signin from './components/Signin';
+import WOW from 'wowjs';
+import Slider from "react-slick";
+
 function App() {
   return (
     <div>
        <BrowserRouter>
         <Switch>
          
-          <Route path="/" exact component={First}></Route>
+          <Route path="/" exact component={Home}></Route>
           <Route exact path="/signin" component={Signin} />
           <Route exact path="/create" component={Create} />
         </Switch>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-function First() {
+// import $ from 'jquery';
+import Slider from "react-slick";
+import WOW from 'wowjs';
+import TypeIt from "typeit-react";
+import HomePage from "../css/homepage.css";
+// import Custom from "../utils/custom.js"
+import Logo from "../images/logo.png";
+
+function Home() {
     return (
-        <div>
-            ddddddddddddddddd
-           <Link to="create">만들기</Link>
-           <Link to="market">탐색하기</Link>
+        <body>
+            <div className="container">
+                <header>
+                    <div className="header-inner">
+                        <div className="logo">
+                            <Link to="/"><img src={Logo}></img></Link>
+                        </div>
+                        <div className='gnb'>
+                        <Link to="/Create">Create</Link>
+                        <Link to="/Market">Create</Link>
+                        <Link to="/Mypage">Create</Link>
+                        <Link to="/Signin">Create</Link>
+                        </div>
+                    </div>
+                </header>
             </div>
+        </body>
     )
 }
 
-export default First
+export default Home
