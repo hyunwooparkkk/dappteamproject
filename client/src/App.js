e1064f4175585e2f512bc12ec26d5222b8082d23
LAPTOP-M723371O\9708j <betty970823@naver.com>
2021-08-26 16:37:47 +0900

    21.08.26 JE 파일 풀하기 전

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,28 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Create from './components/Create';
 import First from './components/Home';
 import Signin from './components/Signin';
+
+import axios from 'axios';
+
 function App() {
-  return (
-    <div>
-       <BrowserRouter>
-        <Switch>
-         
-          <Route path="/" exact component={First}></Route>
-          <Route exact path="/signin" component={Signin} />
-          <Route exact path="/create" component={Create} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  )
+
+  useEffect(() => {
+    axios.get('/api/test')
+    .then(res => console.log(res))
+    .catch()
+    return (
+      <div>
+         <BrowserRouter>
+          <Switch>
+           
+            <Route path="/" exact component={First}></Route>
+            <Route exact path="/signin" component={Signin} />
+            <Route exact path="/create" component={Create} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    )
+  })
 }
 
 export default App
